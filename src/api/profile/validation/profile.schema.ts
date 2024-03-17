f593761648194863812b7e5fc001aa86d6c9baf5
roberto2002-03-12 <robertog-18@hotmail.com>
2024-03-16 23:59:43 -0500

    changing validation on profile

--- src/api/profile/validation/profile.schema.ts
+++ src/api/profile/validation/profile.schema.ts
@@ -3,7 +3,7 @@ import Joi from 'joi';
 const firstName = Joi.string().max(45);
 const lastName = Joi.string().max(45);
 const birth = Joi.date();
-const phoneNumber = Joi.number();
+const phoneNumber = Joi.string().regex(/^[0-9()+\s]+$/).message('El número de teléfono solo puede contener "+", "(", ")", y números');
 
 export const updateProfileSchema = Joi.object({
   firstName: firstName.optional(),
